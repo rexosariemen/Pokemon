e89ac8af04c83e0faf63331b9c368f177df8db01
Rex Osariemen <rexosariemen@gmail.com>
2021-02-23 03:26:07 -0500

    add travisCi configuration for ci/cd pipeline

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -19,7 +19,7 @@ class Pokedex extends Component {
         <h3>Total Experience: {this.props.exp}</h3>
         <div className="Pokedex-cards">
           {this.props.pokemon.map(p => (
-          <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+          <Pokecard key={p.id} id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
         ))}
         </div>
       </div>
